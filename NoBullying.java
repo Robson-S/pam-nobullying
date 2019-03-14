0c43b84e002a09e460f0b051877a1efa4ea169bd
ROBSON SILVA BERNALDO <496050825@alunos.etec.itaqua>
2019-03-14 19:56:26 -0300

    escreci o primeiro passo

--- NoBullying.java
+++ NoBullying.java
@@ -1,1 +1,9 @@
-oi 
+	public class NoBullying {
+	public static void main(String[] args)
+    {   System.out.println('Programa NoBullying');
+	
+	System.out.println("Passo 1: Observa as atitudes do jovem eles mostram sinais e o ser humano não ve.");
+
+	System.out.println("Passo 2: ");
+    } 
+    }
