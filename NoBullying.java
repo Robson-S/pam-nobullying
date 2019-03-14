39776f2c6727c2921c6f2380ba7e98d0848eee9d
ROBSON SILVA BERNALDO <496050825@alunos.etec.itaqua>
2019-03-14 20:00:48 -0300

    escrevi o terceiro passo

--- NoBullying.java
+++ NoBullying.java
@@ -5,5 +5,7 @@
 	System.out.println("Passo 1: Observa as atitudes do jovem eles mostram sinais e o ser humano não ve.");
 
 	System.out.println("Passo 2: Ter mais conversas e menos celular");
+	
+	System.out.println("Passo 3: Ficar mais presente na vida deles");
     } 
     }
